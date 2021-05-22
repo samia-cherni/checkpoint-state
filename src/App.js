a838e6ec102c53b4f1883246e2d9aaeed98e0ba8
samia-cherni <samiacherni@outlook.com>
2021-05-23 00:39:25 +0100

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import React, { Component } from "react";
 import Profile from './components/Profile'
 import Navigation from './components/Navigation'
@@ -8,29 +7,17 @@ export default class App extends Component {
     super(props);
     this.state = {
       Person: {
-        fullName: "Jane Row",
-        bio: "Jane Row  opens the line of communication between clients, customers, and businesses to get projects done. With over 20 years in both public and private sectors, Jane has experience in management consultation, team building, professional development, strategic implementation, and company collaboration. ",
+        fullName: "Jane Roe",
+        bio: "Jane Roe  opens the line of communication between clients, customers, and businesses to get projects done. With over 10 years in both public and private sectors, Jane has experience in management consultation, team building, professional development, strategic implementation, and company collaboration. ",
         imgSrc: "/jane.jpg",
         profession: "Project Manager",
       },
       show: false,
-      seconds: 0,
     };
   }
   toggleShow = () => {
     this.setState({ show: !this.state.show });
   };
-
-  componentDidMount() {
-    this.timer = setInterval(() => {
-      this.setState({ seconds: this.state.seconds + 1 });
-    }, 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timer);
-  }
-
   render() {
     return (
       <div className='App'>
@@ -48,11 +35,6 @@ export default class App extends Component {
                                             />)}
           </div>
           <div className="col-md-3 ml-md-auto">
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-            Seconds elapsed since mount :{this.state.seconds}
-            </li>
-        </ul>
           </div>
         </div>
       </div>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,25 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-const Profile = ({ imgSrc, fullName, bio, profession}) => {
+class Profile extends Component {
+    constructor(){
+        super();
+        this.state={
+            seconds: 0
+        }
+    };
+    componentDidMount() {
+    this.timer = setInterval(() => {
+        this.setState({ seconds: this.state.seconds + 1 });
+    }, 1000);
+    }
+    componentDidUpdate() {
+    console.log('componentDidUpdate()');
+    }
+
+    componentWillUnmount() {
+    clearInterval(this.timer);
+    }
+    render() {
     return (
-    <div>
+        <div>
         <div className="card justify-content-center" style={{ width: "18rem" }}>
-        <img src={imgSrc} className="card-img-top" alt="profile" />
-        <div className="card-body">
-            <h5 className="card-title">{fullName}</h5>
-            <p className="card-text">{bio}</p>
-            <small className="text-muted">{profession}</small>
+            <img src={this.props.imgSrc} className="card-img-top" alt="profile" />
+            <div className="card-body">
+            <h5 className="card-title">{this.props.fullName}</h5>
+            <p className="card-text">{this.props.bio}</p>
+            <small className="text-muted">{this.props.profession}</small>
+            </div>
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                Seconds elapsed since mount :{this.state.seconds}
+                </li>
+            </ul>
         </div>
         </div>
-    </div>
     );
-};
+    }
+}
 
 Profile.propTypes = {
-    fullName : PropTypes.string.isRequired,
-    bio : PropTypes.string.isRequired,
-    profession : PropTypes.string.isRequired,
+    fullName: PropTypes.string.isRequired,
+    bio: PropTypes.string.isRequired,
+    profession: PropTypes.string.isRequired,
 };
 
 export default Profile;
